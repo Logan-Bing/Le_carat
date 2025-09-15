2fca7c225b16b7dc40ef47b80df8e487dd9df18f
Logan <logan@MacBook-De-Logan.local>
2025-09-15 14:15:16 +0200

    animated about page

---
+++ le_carat/core/static/core/js/script.js
@@ -0,0 +1,60 @@
+gsap.registerPlugin(ScrollTrigger)
+
+// TARGET DES ELEMENTS HTML
+const main_about = document.querySelector(".main__about");
+const about_title = document.querySelector(".about__content__title");
+const about_paragraph = document.querySelector(".about__content__paragraph");
+
+// CREATION DES CONTAINERS POUR ACCEUILLIR LES SPAN
+const title_container = document.createElement("div");
+const paragraph_container = document.createElement("div");
+title_container.classList.add("word");
+paragraph_container.classList.add("word");
+
+// FONCTION POUR SPLIT L'ELEMENT HTML EN SPAN
+function split_into_span(element)
+{
+    let words = element.textContent.split(" ");
+    words = words.map(word => {
+        let letters = word.split('');
+        letters = letters.map(letter => `<span class="char">${letter}</span>`);
+        return letters.join('');
+    });
+    return words;
+}
+
+// FONCTION POUR INSERER LES ELEMENT SPLITTER DANS LES CONTAINERS
+function insert_in_div(parent_element, child_element, div_container)
+{
+    div_container.innerHTML = child_element.join(" ");
+    parent_element.innerHTML = div_container.outerHTML;
+}
+
+let splited_title = split_into_span(about_title);
+let splited_paragraph = split_into_span(about_paragraph);
+
+insert_in_div(about_title, splited_title, title_container);
+insert_in_div(about_paragraph, splited_paragraph, paragraph_container);
+
+// ANIMATIONS GSAP
+
+const tl = gsap.timeline({
+    defaults : {
+        y: 0,
+        delay: 0.2,
+        duration: .1,
+    },
+    scrollTrigger: {
+        trigger: main_about,
+        start: "top 60%",
+        markers: true,
+    }
+});
+
+tl.to(about_title.querySelectorAll('.char'), {
+    stagger: 0.05,
+    delay: 0.2,
+})
+.to(about_paragraph.querySelectorAll('.char'),{
+    stagger: 0.02,
+})
